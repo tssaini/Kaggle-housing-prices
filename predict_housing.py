775c3e52e0b97fa262d06280581bca22b0719b43
Taranpreet Saini <taranpreet_saini@manulife.com>
2020-02-29 00:22:32 -0500

    xgboost model added

--- predict_housing.py
+++ predict_housing.py
@@ -10,9 +10,12 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.impute import SimpleImputer
+from xgboost import XGBRegressor
 
 
 HOUSING_TRAIN_DATA = "train.csv" 
@@ -31,7 +34,7 @@ def transformData(housing_data):
     return train_set_X, train_set_y
 
 
-if __name__ == "__main__":
+def attempt1():
     train_val_set, test_set = load_housing_data()
 
     X, y = transformData(train_val_set)
@@ -53,7 +56,7 @@ if __name__ == "__main__":
     lin_reg = LinearRegression()
     lin_reg.fit(train_set_X_prepared, y_train)
 
-    #RMSE
+    #MSE
     housing_predictions = lin_reg.predict(val_set_X_prepared)
     lin_mse = mean_squared_error(y_val, housing_predictions)
     lin_rmse = np.sqrt(lin_mse)
@@ -68,3 +71,30 @@ if __name__ == "__main__":
     tree_rmse = np.sqrt(tree_mse)
     print("Decision Tree: ",tree_rmse)
 
+
+
+
+def attempt2():
+
+    data = pd.read_csv("train.csv")
+    data.dropna(axis=0, subset=['SalePrice'], inplace=True)
+    y = data.SalePrice
+    X = data.drop(['SalePrice'], axis=1).select_dtypes(exclude=['object'])
+
+    train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.2)
+
+    imputer = SimpleImputer(strategy="median")
+    train_X = imputer.fit_transform(train_X)
+    test_X = imputer.transform(test_X)
+
+    my_model = XGBRegressor(n_estimators=1000)
+    my_model.fit(train_X, train_y, early_stopping_rounds=5, eval_set=[(test_X, test_y)], verbose=False)
+
+    # make predictions
+    predictions = my_model.predict(test_X)
+    print("XGBoost : ", np.sqrt(mean_squared_error(predictions, test_y)))
+
+
+if __name__ == "__main__":
+    attempt1()
+    attempt2()
